b279fed6f1eda16ff0235815dcfe32679a130f89
Gourab Bank <92862944+gourabbank@users.noreply.github.com>
2026-04-12 17:24:41 -0400

    Add: open-the-lock - submission-3

---
+++ Data Structures & Algorithms/open-the-lock/submission-3.java
@@ -0,0 +1,43 @@
+class Solution {
+    public int openLock(String[] deadends, String target) {
+        Set<String> visit=new HashSet<>(Arrays.asList(deadends));
+        if(visit.contains("0000")) return -1;
+        visit.add("0000");
+        Queue<String> q=new LinkedList<>();
+        q.offer("0000");
+        int steps=0;
+        while(!q.isEmpty())
+        {
+            int size=q.size();
+            for(int i=0;i<size;i++)
+            {
+                String lock=q.poll();
+                if(lock.equals(target)) return steps;
+                for(String next:children(lock))
+                {
+                    if(!visit.contains(next))
+                    {
+                        q.offer(next);
+                        visit.add(next);
+                    }
+                }
+            }
+            steps++;
+        }
+        return -1;
+    }
+    public List<String> children(String lock)
+    {
+        List<String> res=new ArrayList<>();
+        for(int i=0;i<4;i++)
+        {
+            char[] arr=lock.toCharArray();
+            arr[i]=(char)(((arr[i]-'0'+1)%10)+'0');
+            res.add(new String(arr));
+            arr=lock.toCharArray();
+            arr[i]=(char)(((arr[i]-'0'-1+10)%10)+'0');
+            res.add(new String(arr));
+        }
+        return res;
+    }
+}
