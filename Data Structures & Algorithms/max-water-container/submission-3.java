4babf799cbb338553a0eee904ee8d9be09582923
Gourab Bank <92862944+gourabbank@users.noreply.github.com>
2026-04-27 17:06:19 -0400

    Add: max-water-container - submission-3

---
+++ Data Structures & Algorithms/max-water-container/submission-3.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int maxArea(int[] heights) {
+        int l=0;
+        int r=heights.length-1;
+        int max=0;
+        while(l<=r)
+        {
+            int water=Math.min(heights[l],heights[r]) * (r-l);
+            if(heights[l]<heights[r]) l++;
+            else r--;
+            max=Math.max(water,max);
+        }
+        return max;
+    }
+}
