8079d027d4587eeafcb06f1e443f06bccbf079b8
Gourab Bank <92862944+gourabbank@users.noreply.github.com>
2026-04-27 14:21:19 -0400

    Add: meeting-schedule - submission-6

---
+++ Data Structures & Algorithms/meeting-schedule/submission-6.java
@@ -0,0 +1,24 @@
+/**
+ * Definition of Interval:
+ * public class Interval {
+ *     public int start, end;
+ *     public Interval(int start, int end) {
+ *         this.start = start;
+ *         this.end = end;
+ *     }
+ * }
+ */
+
+class Solution {
+    public boolean canAttendMeetings(List<Interval> intervals) {
+        if(intervals.size()<2) return true;
+        Collections.sort(intervals,(a,b)->(a.start-b.start));
+        Interval prev=intervals.get(0);
+        for(int i=1;i<intervals.size();i++)
+        {
+            if(intervals.get(i).start<prev.end) return false;
+            prev=intervals.get(i);
+        }
+        return true;
+    }
+}
