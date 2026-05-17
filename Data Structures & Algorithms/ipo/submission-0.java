e8b173b8c25dbb385eacb8558de0495f8e067211
Gourab Bank <92862944+gourabbank@users.noreply.github.com>
2026-05-16 20:01:02 -0400

    Add: ipo - submission-0

---
+++ Data Structures & Algorithms/ipo/submission-0.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int findMaximizedCapital(int k, int w, int[] profits, int[] capital) {
+        int n=profits.length;
+        PriorityQueue<int[]> minCap=new PriorityQueue<>((a,b)->a[0]-b[0]);
+        PriorityQueue<Integer> maxProfit=new PriorityQueue<>((a,b)->b-a);
+        for(int i=0;i<n;i++)
+        {
+            minCap.offer(new int[]{capital[i],profits[i]});
+        }
+        for(int i=0;i<k;i++)
+        {
+            while(!minCap.isEmpty() && minCap.peek()[0]<=w)
+            {
+                maxProfit.offer(minCap.poll()[1]);
+            }
+            if(maxProfit.isEmpty()) break;
+            w+=maxProfit.poll();
+        }
+        return w;
+    }
+}
