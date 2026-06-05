bd987aeafaba5409869b7bf86915b4842e42c8c0
Gourab Bank <92862944+gourabbank@users.noreply.github.com>
2026-06-05 16:23:35 -0400

    Add: is-anagram - submission-7

---
+++ Data Structures & Algorithms/is-anagram/submission-7.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s.length()!=t.length())return false;
+        int[] a=new int[26];
+        for(char ch:s.toCharArray())
+        {
+            a[ch-'a']++;
+        }
+        for(char ch:t.toCharArray())
+        {
+            a[ch-'a']--;
+        }
+        for(int n:a) if(n!=0) return false;
+        return true;
+    }
+}
