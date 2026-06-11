1190c09024528a6c7b203e6f80ff33d96170baef
Gourab Bank <92862944+gourabbank@users.noreply.github.com>
2026-06-11 13:23:30 -0400

    Add: contains-duplicate-ii - submission-5

---
+++ Data Structures & Algorithms/contains-duplicate-ii/submission-5.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        int l=0;
+        HashSet<Integer> set=new HashSet<>();
+        for(int r=0;r<nums.length;r++)
+        {
+            if(r-l>k)
+            {
+                set.remove(nums[l]);
+                l++;
+            }
+            if(set.contains(nums[r])) return true;
+            set.add(nums[r]);
+        }
+        return false;
+    }
+}
