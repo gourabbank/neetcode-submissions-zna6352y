be9738b0325adff35ae82d2d61c0aac862121195
Gourab Bank <92862944+gourabbank@users.noreply.github.com>
2026-06-11 10:13:07 -0400

    Add: valid-palindrome-ii - submission-2

---
+++ Data Structures & Algorithms/valid-palindrome-ii/submission-2.java
@@ -0,0 +1,29 @@
+class Solution {
+    public boolean validPalindrome(String s) {
+        int k=0;
+        int l=0,r=s.length()-1;
+        while(l<r)
+        {
+            if(s.charAt(l)==s.charAt(r))
+            {
+                l++;
+                r--;
+            }
+            else
+            {
+                return isPalindrome(s,l+1,r) || isPalindrome(s,l,r-1);
+            }
+        }
+        return true;
+    }
+    public boolean isPalindrome(String s, int l , int r)
+    {
+        while(l<r)
+        {
+            if(s.charAt(l)!=s.charAt(r)) return false;
+            l++;
+            r--;
+        }
+        return true;
+    }
+}
