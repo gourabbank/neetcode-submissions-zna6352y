e3033d0d45f4088a81a0e7ca76c6437cb28dc658
Gourab Bank <92862944+gourabbank@users.noreply.github.com>
2026-06-21 14:28:50 -0400

    Add: find-duplicate-integer - submission-4

---
+++ Data Structures & Algorithms/find-duplicate-integer/submission-4.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int findDuplicate(int[] nums) {
+        int sum=0;
+        for(int i=0;i<nums.length;i++)
+        {
+            int idx=Math.abs(nums[i])-1;
+            if(nums[idx]<0)
+            {
+                return Math.abs(nums[i]);
+            }
+            nums[idx]=(-1)*nums[idx];
+        }
+        return -1;
+    }
+}
